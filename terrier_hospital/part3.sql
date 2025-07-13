baf86be462cfc51d711aadf5f9dec5b5f6ebd63c
medprod <medhaa.prodduturi@gmail.com>
2025-07-13 01:19:49 -0500

    3

--- terrier_hospital/part3.sql
+++ terrier_hospital/part3.sql
@@ -1,11 +1,11 @@
 SET search_path = terrier_hospital;
 
 --8. aggregates, joins, group by
-SELECT * FROM blood_test
-SELECT * from employee
-SELECT * FROM Person
-SELECT * FROM department
-SELECT * FROM hospital
+SELECT * FROM blood_test;
+SELECT * from employee;
+SELECT * FROM Person;
+SELECT * FROM department;
+SELECT * FROM hospital;
 
 SELECT 
 pe.first_name || ' ' || pe.last_name AS employee_name,
@@ -18,15 +18,15 @@ JOIN Person pe ON e.person_id = pe.person_id
 JOIN department d ON e.department_id = d.department_id
 JOIN hospital h ON d.hospital_id = h.hospital_id
 GROUP BY employee_name, department_name, hospital_name
-HAVING COUNT(*) >= 5
+HAVING COUNT(*) >= 5;
 
 
 --9. aggregates, date differences, grouping
-SELECT * from employee
-SELECT * FROM Person
-SELECT * FROM doctor
-SELECT * FROM appointment
-SELECT * FROM appointment_type
+SELECT * from employee;
+SELECT * FROM Person;
+SELECT * FROM doctor;
+SELECT * FROM appointment;
+SELECT * FROM appointment_type;
 
 SELECT 
 pe.first_name || ' ' || pe.last_name AS Doctor_Name,
@@ -39,7 +39,7 @@ JOIN Employee e ON d.employee_id = e.employee_id
 JOIN Person pe ON e.person_id = pe.person_id
 LEFT JOIN appointment_type atp ON a.appointment_type_id = atp.appointment_type_id
 WHERE date_part('year', a.scheduled_for) = 2023
-GROUP BY Doctor_Name, atp.type, date_part('month', a.scheduled_for)
+GROUP BY Doctor_Name, atp.type, date_part('month', a.scheduled_for);
 
 
 
