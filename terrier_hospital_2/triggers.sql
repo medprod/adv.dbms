7a76501d016385574a839647a47688f267c5294f
medprod <medhaa.prodduturi@gmail.com>
2025-07-19 22:31:39 -0500

    trigger

--- terrier_hospital_2/triggers.sql
+++ terrier_hospital_2/triggers.sql
@@ -25,6 +25,101 @@ DELETE FROM appointment_history
 WHERE appointment_id = 611;
 
 
+--3. trigger that maintains the appointment based on the transactions made to the appointment table
+SELECT * FROM appointment_history
+order by appointment_id;
 
---2. trigger that maintains the appointment based on the transactions made to the appointment table
+--defining the trigger function
+CREATE OR REPLACE FUNCTION appointment_history_trigger()
+RETURNS TRIGGER AS $$
+BEGIN
+	IF trigger_operation = 'INSERT' OR 'UPDATE' THEN
+		INSERT INTO appointment_history(
+		    update_type, updated_at,
+		    appointment_id, scheduled_for, patient_concern,
+		    appointment_type_id, appointment_type,
+		    patient_id, patient_first_name, patient_last_name,
+		    doctor_id, doctor_first_name, doctor_last_name,
+		    hospital_id, hospital_name,
+		    appointment_status_id, appointment_status,
+		    diagnosis_code, diagnosis_name,
+		    lab_id, patient_vitals_id, medicine_id
+		)
+		SELECT 
+			CASE WHEN trigger_operation = 'INSERT' THEN 'NEW INSERT' ELSE 'UPDATE' END AS update_type,
+            CURRENT_TIMESTAMP, a.appointment_id, a.scheduled_for, a.patient_concern, a.appointment_type_id,
+            at.type AS appointment_type, p.patient_id, per.first_name, per.last_name, d.doctor_id,
+            doc_person.first_name, doc_person.last_name, h.hospital_id,h.name,
+            ast.appointment_status_id,ast.status, ad.diagnosis_code, diag.diagnosis AS diagnosis_name,
+            l.lab_id, pv.patient_vitals_id, m.medicine_id
+        FROM appointment a
+        JOIN patient p ON a.patient_id = p.patient_id
+        JOIN person per ON p.person_id = per.person_id
+        JOIN doctor d ON a.doctor_id = d.doctor_id
+        JOIN employee e ON d.employee_id = e.employee_id
+        JOIN person doc_person ON e.person_id = doc_person.person_id
+        JOIN hospital h ON a.hospital_id = h.hospital_id
+        JOIN appointment_type at ON a.appointment_type_id = at.appointment_type_id
+        JOIN appointment_status ast ON a.appointment_status_id = ast.appointment_status_id
+        LEFT JOIN appointment_diagnosis ad ON a.appointment_id = ad.appointment_id
+        LEFT JOIN diagnosis diag ON ad.diagnosis_code = diag.diagnosis_code
+        LEFT JOIN lab l ON a.lab_id = l.lab_id
+        LEFT JOIN patient_vitals pv ON a.patient_vitals_id = pv.patient_vitals_id
+        LEFT JOIN appointment_prescription ap ON a.appointment_id = ap.appointment_id
+        LEFT JOIN prescription pr ON ap.prescription_id = pr.prescription_id
+        LEFT JOIN medicine m ON pr.medicine_id = m.medicine_id
+        WHERE a.appointment_id = NEW.appointment_id;
+        RETURN NEW;
+	ELSEIF trigger_operation = 'DELETE' THEN
+		INSERT INTO appointment_history(
+		    update_type, updated_at,
+		    appointment_id, scheduled_for, patient_concern,
+		    appointment_type_id, appointment_type,
+		    patient_id, patient_first_name, patient_last_name,
+		    doctor_id, doctor_first_name, doctor_last_name,
+		    hospital_id, hospital_name,
+		    appointment_status_id, appointment_status,
+		    diagnosis_code, diagnosis_name,
+		    lab_id, patient_vitals_id, medicine_id
+		)
+		SELECT 
+			'DELETE',
+            CURRENT_TIMESTAMP, a.appointment_id, a.scheduled_for, a.patient_concern, a.appointment_type_id,
+            at.type AS appointment_type, p.patient_id, per.first_name, per.last_name, d.doctor_id,
+            doc_person.first_name, doc_person.last_name, h.hospital_id,h.name,
+            ast.appointment_status_id,ast.status, ad.diagnosis_code, diag.diagnosis AS diagnosis_name,
+            l.lab_id, pv.patient_vitals_id, m.medicine_id
+        FROM appointment a
+        JOIN patient p ON a.patient_id = p.patient_id
+        JOIN person per ON p.person_id = per.person_id
+        JOIN doctor d ON a.doctor_id = d.doctor_id
+        JOIN employee e ON d.employee_id = e.employee_id
+        JOIN person doc_person ON e.person_id = doc_person.person_id
+        JOIN hospital h ON a.hospital_id = h.hospital_id
+        JOIN appointment_type at ON a.appointment_type_id = at.appointment_type_id
+        JOIN appointment_status ast ON a.appointment_status_id = ast.appointment_status_id
+        LEFT JOIN appointment_diagnosis ad ON a.appointment_id = ad.appointment_id
+        LEFT JOIN diagnosis diag ON ad.diagnosis_code = diag.diagnosis_code
+        LEFT JOIN lab l ON a.lab_id = l.lab_id
+        LEFT JOIN patient_vitals pv ON a.patient_vitals_id = pv.patient_vitals_id
+        LEFT JOIN appointment_prescription ap ON a.appointment_id = ap.appointment_id
+        LEFT JOIN prescription pr ON ap.prescription_id = pr.prescription_id
+        LEFT JOIN medicine m ON pr.medicine_id = m.medicine_id
+        WHERE a.appointment_id = OLD.appointment_id;
+        RETURN OLD;
+	END IF;
+	RETURN NULL;
+END;
+$$
+LANGUAGE PLPGSQL;
+
+--creating the trigger
+CREATE TRIGGER trigger_appointment_history
+AFTER INSERT OR UPDATE OR DELETE ON appointment
+FOR EACH ROW
+EXECUTE FUNCTION appointment_history_trigger();
+
+
+--test case: insert
+
 
